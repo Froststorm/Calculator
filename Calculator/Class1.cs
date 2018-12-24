19b80b547a5a9c13ad966e1eb68c065ba4ef160f
Andrii <andrey.paladin@gmail.com>
2018-12-25 00:43:53 +0200

    Add project files.

---
+++ Calculator/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public static int Additioning(int num, int num1)
+        {
+            return num + num1;
+        }
+
+        public static int Substraction(int num, int num1)
+        {
+            return num - num1;
+        }
+
+        public static double Division(double num, double num1)
+        {
+            return num/num1;
+        }
+
+        public static double Multiplication (double num, double num1)
+        {
+            return num * num1;
+        }
+
+        public static double PowerOfTwo(double num)
+        {
+            return num * num ;
+        }
+
+        public static double Squarerooting(double num)
+        {
+            num = Math.Sqrt(num);
+            return num;
+        }
+    }
+}
